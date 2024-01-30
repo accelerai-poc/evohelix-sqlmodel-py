1ab0b99f84efe6f31534f6a9d605383f671eaa1c
Lukas Loock <lloock@accelerai.de>
2024-01-30 14:29:31 +0100

    removed rest functions, added migrations section in README

--- README.md
+++ README.md
@@ -5,3 +5,35 @@
 ```bash
 pip install 'evohelix_sqlm @ git+https://github.com/accelerai-poc/evohelix-sqlmodel-py@main'
 ```
+
+## Usage
+
+- create data model: setup your data model classes in pydantic-like manner:
+```python
+from typing import Optional
+from sqlmodel import Field, SQLModel
+
+class MyModel(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    my_first_att: str
+    my_second_optional_att:  Optional[int] = None
+```
+
+## Migrations using Alembic
+1. prepare migrations by adding your data model classes to `alembic/env.py` and setting the `sqlalchemy.url` (see `evohelix-service-template`):
+```python
+from myapp.models import MyModel # <-- add this line in import section before running any code
+from alembic import context
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+config.set_main_option("sqlalchemy.url", settings.SQLALCHEMY_DATABASE_URI) # need to set 'SQLALCHEMY_DATABASE_URI' in settings.py
+```
+3. run migration bash inside container or from host with docker compose:
+```bash
+alembic upgrade head
+``` 
+```bash
+docker compose exec web alembic upgrade head
+``` 
---
+++ src/evohelix_sqlm/utils.py
@@ -0,0 +1,43 @@
+import re
+from sqlmodel import col
+
+# Parse complex query parameters into a filter system
+def parse_filters(model, params):
+    where = []
+    order_by = None
+    offset = None
+    limit = None
+
+    def parse_param(param, value):
+        if m := re.search(r"(?P<key>.*)\[(?P<op>.*)\]", param):
+            return {"key": m.group("key"), "op": m.group("op"), "val": value}
+        return {"key": param, "op": "eq", "val": value}
+
+    for k, v in params.items():
+        if k == 'order_by':
+            if v not in model.__dict__.keys():
+                raise KeyError(f'Order key {v} is not part of the {model} model.')
+            order_by = v
+        elif k == 'offset':
+            offset = v
+        elif k == 'limit':
+            limit = v
+        else:
+            condition = parse_param(k, v)
+            if condition["key"] not in model.__dict__.keys():
+                raise KeyError(f'Filter key {condition["key"]} is not part of the {model} model.')
+            if condition["op"] == "gt":
+                where.append(getattr(model, condition["key"]) > condition["val"])
+            elif condition["op"] == "gte":
+                where.append(getattr(model, condition["key"]) >= condition["val"])
+            elif condition["op"] == "lt":
+                where.append(getattr(model, condition["key"]) < condition["val"])
+            elif condition["op"] == "lte":
+                where.append(getattr(model, condition["key"]) <= condition["val"])
+            elif condition["op"] == "eq":
+                where.append(getattr(model, condition["key"]) == condition["val"])
+            elif condition["op"] == "neq":
+                where.append(getattr(model, condition["key"]) != condition["val"])
+            elif condition["op"] == "like":
+                where.append(col(getattr(model, condition["key"])).contains(condition["val"]))
+    return where, order_by, limit, offset
